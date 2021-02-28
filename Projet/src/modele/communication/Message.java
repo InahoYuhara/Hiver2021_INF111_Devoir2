4c9e6fa027b8ac39dff6db97541055ada0320ab7
Nao <inahoyuhara@gmail.com>
2021-02-28 14:16:42 -0500

    Ajout d'attributs et de methodes pour la classe Message

--- Projet/src/modele/communication/Message.java
+++ Projet/src/modele/communication/Message.java
@@ -1,4 +1,21 @@
 package modele.communication;
 
 public class Message {
+
+    String numeroDestination;
+    String message;
+
+    public Message(String numeroDestination, String message){
+        this.numeroDestination = numeroDestination;
+        this.message = message;
+    }
+
+    public String getNumeroDestination(){
+        return this.numeroDestination;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+
 }
