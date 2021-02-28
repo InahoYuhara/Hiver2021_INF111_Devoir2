e954e2f3cd6cbf372dcaa1cc56c62bac158029d3
Nao <inahoyuhara@gmail.com>
2021-02-28 14:04:05 -0500

    Ajout dattributs et de methodes pour la classe connexion

--- Projet/src/modele/communication/Connexion.java
+++ Projet/src/modele/communication/Connexion.java
@@ -1,4 +1,29 @@
 package modele.communication;
 
+import modele.reseau.Antenne;
+
 public class Connexion {
+
+    public final int NB_ANTENNES = 2;
+    int numeroConnexion;
+    Antenne tableauAntenne[];
+
+    public Connexion(int numeroConnexion, Antenne antenne1, Antenne antenne2){
+
+    }
+
+    public int getNumeroConnexion(){
+        return this.numeroConnexion;
+    }
+
+    public boolean equals(int numeroConnexion){
+        if (this.numeroConnexion == numeroConnexion){
+            return true;
+        }
+        return false;
+    }
+
+
+
+
 }
